16a86b7949afad6717e97a989c65915508308409
sharpaccount <vladikinastadiki@gmail.com>
2023-12-06 16:09:33 +0300

    started working

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaTilda.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaTilda.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
