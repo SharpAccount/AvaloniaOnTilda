9c472aad9d6d0d9dd6ab1e6635dbbea2af403878
sharpaccount <vladikinastadiki@gmail.com>
2023-12-06 17:21:38 +0300

    added reg page

---
+++ Views/SignUp.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaTilda.Views;
+
+public partial class SignUp : Window
+{
+    public SignUp()
+    {
+        InitializeComponent();
+    }
+}
