eb69f0d838ec266d28f5fa17d8487a808d083106
sharpaccount <vladikinastadiki@gmail.com>
2023-12-10 23:47:32 +0300

    added login and email confirm pages

---
+++ Views/EmailConfirm.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaTilda.Views;
+
+public partial class EmailConfirm : Window
+{
+    public EmailConfirm()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/SignIn.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaTilda.Views;
+
+public partial class SignIn : Window
+{
+    public SignIn()
+    {
+        InitializeComponent();
+    }
+}
