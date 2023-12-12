a8575b546f68403657f6024415b092635cb8bf19
sharpaccount <vladikinastadiki@gmail.com>
2023-12-12 16:59:08 +0300

    made signing up and in pages, statrted to make a routing

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace AvaloniaTilda.ViewModels;
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace AvaloniaTilda.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private readonly Dictionary<int, Control> Pages = new Dictionary<int, Control>
+    {
+        
+    };
     public string Greeting => "Welcome to Avalonia!";
 }
