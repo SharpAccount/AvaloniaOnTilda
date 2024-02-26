84d162667bb101c40767d1de60054910381920ff
sharpaccount <vladikinastadiki@gmail.com>
2024-02-26 22:19:43 +0300

    fixed some bugs

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using AvaloniaTilda.Models;
 using AvaloniaTilda.ViewModels;
@@ -11,10 +14,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
 
     private void OpenShowUsers(string login, string email, string password)
     {
         //open showusers(ObservableCollection<> =  new ObservableCollection<> {login, email, pass})
+    }
+
+    public void foo(object? sender, PointerPressedEventArgs args)
+    {
+        ListBoxItem items;
     }
 }
